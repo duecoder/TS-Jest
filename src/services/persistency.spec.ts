340c437919f6c28756b40aeaea71299af9a2984f
duecoder <eduardodecastro@outlook.com>
2022-08-31 11:37:26 -0300

    first commit

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,9 @@
+it('IT test description', () => {
+  const number = 1;
+  expect(number).toBe(1);
+});
+
+test('TEST description', () => {
+  const nome = 'Eduardo';
+  expect(nome).toBe('Maria');
+});
